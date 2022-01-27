6de27981778cfa0f8b671768ff955bfa3c19739c
ironsj <jakeirons007@gmail.com>
2022-01-26 23:02:47 -0500

    adding MvcMovie project

---
+++ MvcMovie/MvcMovie/Data/SeedData.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Models;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Wizzard of Oz",
+                        ReleaseDate = DateTime.Parse("1939-8-15"),
+                        Genre = "Family",
+                        Price = 2.78M,
+                        Rating = "PG"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Dr. Bean",
+                        ReleaseDate = DateTime.Parse("1997-8-2"),
+                        Genre = "Comedy",
+                        Price = 18.00M,
+                        Rating = "PG"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Sound of Music",
+                        ReleaseDate = DateTime.Parse("1965-3-2"),
+                        Genre = "Musical",
+                        Price = 8.20M,
+                        Rating = "PG"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Chariots of Fire",
+                        ReleaseDate = DateTime.Parse("1981-4-15"),
+                        Genre = "Historical Drama",
+                        Price = 5.50M,
+                        Rating = "PG"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Amadeus",
+                        ReleaseDate = DateTime.Parse("1984-9-19"),
+                        Genre = "Biographical Drama",
+                        Price = 17.99M,
+                        Rating = "PG"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        #region
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        #endregion
+        public string Title { get; set; }
+        #region
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        #endregion
+        public DateTime ReleaseDate { get; set; }
+        #region
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        #endregion
+        public decimal Price { get; set; }
+        #region
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        #endregion
+        public string Genre { get; set; }
+        #region
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        #endregion
+        public string Rating { get; set; }
+    }
+}
